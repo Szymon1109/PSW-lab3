b3e8fbcc1ca2860777b5dbd1096a672e2c0690eb
Szymon1109 <35738498+Szymon1109@users.noreply.github.com>
2019-03-19 16:28:14 +0100

    Update RGBController.java

--- RGBController.java
+++ RGBController.java
@@ -1,29 +1,29 @@
 public class RGBController {
-	public static RGB mixed = new RGB();
 	
 	public static void wyswietl(RGB rgb) {	
 		System.out.print("Składowe barwy '" + rgb.toString() + "': ");
 		System.out.print("[" + rgb.getR_value() + ", " + rgb.getG_value() + ", " + rgb.getB_value() + "]\n");
 	}
 	
-	public static void mieszaj(RGB rgb1, RGB rgb2) {	
-		int R_value = Math.min(rgb1.getR_value() + rgb2.getR_value(), 255);
-		int G_value = Math.min(rgb1.getR_value() + rgb2.getR_value(), 255);
-		int B_value = Math.min(rgb1.getR_value() + rgb2.getR_value(), 255);
+	public static void mieszaj(RGB rgb1, RGB rgb2, RGB mixed) {	
+		double R_value = Math.round((rgb1.getR_value() + rgb2.getR_value()) / 2.0);
+		double G_value = Math.round((rgb1.getG_value() + rgb2.getG_value()) / 2.0);
+		double B_value = Math.round((rgb1.getB_value() + rgb2.getB_value()) / 2.0);
 		
-		mixed.setR_value(R_value);
-		mixed.setG_value(G_value);
-		mixed.setB_value(B_value);
+		mixed.setR_value((int)R_value);
+		mixed.setG_value((int)G_value);
+		mixed.setB_value((int)B_value);
 	}
 
 	public static void main(String[] args) {
 		RGB white = new RGB();
 		RGB red = new RGB(255, 0, 0);
+		RGB mixed = new RGB();
 		
 		wyswietl(white);
 		wyswietl(red);
 		
-		mieszaj(white, red);
+		mieszaj(white, red, mixed);
 		wyswietl(mixed);
 	}
 }
